81a99fc6790af92daa6122734ff8e6ce1eeaf568
73735 <rabbitmilkman@gmail.com>
2021-10-07 12:53:03 +0900

    first commit

---
+++ js/d3/treedata.js
@@ -0,0 +1,134 @@
+var treeData = [{
+	"name": "Home",
+	"parent": "null",
+	"children": [{
+		"name": "Who We Are",
+		"children": [{
+			"name": "Mission & Vision"
+		}, {
+			"name": "Board of Directors"
+		}, {
+			"name": "20-20 Vision for Food Insecurity"
+		}, {
+			"name": "Careers"
+		}, {
+			"name": "Contact Us"
+		}]
+	}, {
+		"name": "Find Food",
+		"children": [{
+			"name": "Find Food"
+		}, {
+			"name": "New Toronto Street Food Bank"
+		}, {
+			"name": "Food Bank Users FAQs"
+		}]
+	}, {
+		"name": "Donate",
+		"children": [{
+			"name": "Individual Donations",
+			"children": [{
+				"name": "One-time Donation"
+			}, {
+				"name": "Giftcard Donation"
+			}, {
+				"name": "Monthly Donations"
+			}, {
+				"name": "Securities Donations"
+			}, {
+				"name": "Leave a Gift in Your Will"
+			}]
+		}, {
+			"name": "Corporate Donations",
+			"children": [{
+				"name": "Corporate Donations"
+			}, {
+				"name": "Create a Campaign"
+			}, {
+				"name": "Employee Matching Program"
+			}]
+		}, {
+			"name": "Farm Donations",
+			"children": [{
+				"name": "Farm to Food Bank Program"
+			}, {
+				"name": "Good Samaritan Act"
+			}, {
+				"name": "Tax Receipting for Farmers"
+			}]
+		}, {
+			"name": "Food Donations"
+		}, {
+			"name": "Donors & Supporters"
+		}, {
+			"name": "Sounds of the Season"
+		}]
+	}, {
+		"name": "Volunteer & Fundraise",
+		"children": [{
+			"name": "Individual Volunteers",
+			"children": [{
+				"name": "Individual Volunteers"
+			}, {
+				"name": "Student Group Volunteering"
+			}, {
+				"name": "Take Action Project for High School Student Volunteers"
+			}, {
+				"name": "Public Food Sports & Family Volunteering"
+			}]
+		}, {
+			"name": "Corporate Volunteers",
+			"children": [{
+				"name": "Volunteer in Our Facility"
+			}, {
+				"name": "Employee Volunteer Program"
+			}, {
+				"name": "Food Sort Challenge for Corporations"
+			}]
+		}, {
+			"name": "Fundraising",
+			"children": [{
+				"name": "Host a Food Drive or Fundraiser"
+			}, {
+				"name": "Fundraising Toolkit"
+			}, {
+				"name": "Community Event Partnership"
+			}]
+		}, {
+			"name": "Events Calender"
+		}]
+	}, {
+		"name": "What Are We Up To",
+		"children": [{
+			"name": "Daily Bread Blog"
+		}, {
+			"name": "News & Media Releases"
+		}, {
+			"name": "Research & Advocacy",
+			"children": [{
+				"name": "Advocacy"
+			}, {
+				"name": "Budget Submissions"
+			}, {
+				"name": "Who's Hungry Report"
+			}, {
+				"name": "Policy Updates"
+			}, {
+				"name": "Affordable Housing Town Hall"
+			}]
+		}, {
+			"name": "Best in Class Awards"
+		}, {
+			"name": "Events Calender"
+		}]
+	}, {
+		"name": "Community Agencies",
+		"children": [{
+			"name": "Community Partner Agencies"
+		}, {
+			"name": "Training for Agencies"
+		}, {
+			"name": "Training for Agencies FAQs"
+		}]
+	}]
+}];
---
+++ ucisd/treedata.js
@@ -0,0 +1,489 @@
+var treeData = [{
+	"name": "UCISD",
+	"parent": "null",
+	"children": [{
+		"name": "Dan Ryan",
+		"children": [{
+			"name": "Do",
+			"children": [{
+				"name": "Teach UCISD"
+			}, {
+				"name": "Emphasize teamwork"
+			}, {
+				"name": "Emphasize professionalism"
+			}, {
+				"name": "Move around a lot"
+			}, {
+				"name": "Keep assignments intentionally vague"
+			}, {
+				"name": "Like NPR podcasts"
+			}]
+		}, {
+			"name": "Think",
+			"children": [{
+				"name": "Worry about what students think"
+			}]
+		}, {
+			"name": "Feel",
+			"children": [{
+				"name": "Unsure"
+			}]
+		}]
+	}, {
+		"name": "Course",
+		"children": [{
+			"name": "Teamwork",
+			"children": [{
+				"name": "Visual Telephone"
+			}, {
+				"name": "Six Hats",
+				"children": [{
+					"name": "Cultivate Creative Listning"
+				}]
+			}]
+		}, {
+			"name": "Users, Design, Information System",
+			"children": [{
+				"name": "Human-Centerd Design",
+				"children": [{
+					"name": "[W] who need to"
+				}, {
+					"name": "[X]"
+				}, {
+					"name": "would pay for a [Y]"
+				}, {
+					"name": "that helps by [Z]"
+				}]
+			}, {
+				"name": "Journal (verb)",
+				"children": [{
+					"name": "Habits of Mind"
+				}, {
+					"name": "Habits of Work"
+				}]
+			}]
+		}, {
+			"name": "HCD as Creative Problem Solving",
+			"children": [{
+				"name": "Intentionality"
+			}, {
+				"name": "Probrem Solving",
+				"children": [{
+					"name": "Conventional",
+					"children": [{
+						"name": "Rationality"
+					}, {
+						"name": "Optimization"
+					}, {
+						"name": "Efficiency"
+					}]
+				}, {
+					"name": "Creative",
+					"children": [{
+						"name": "Steps",
+						"children": [{
+							"name": "Accept"
+						}, {
+							"name": "Analyze"
+						}, {
+							"name": "Define"
+						}, {
+							"name": "Ideate"
+						}, {
+							"name": "Select"
+						}, {
+							"name": "Implement"
+						}, {
+							"name": "Evaluate"
+						}]
+					}, {
+						"name": "Methods",
+						"children": [{
+							"name": "Five Whys"
+						}, {
+							"name": "Happiness Is"
+						}, {
+							"name": "Dump Your Aces"
+						}, {
+							"name": "Forced Analogy"
+						}, {
+							"name": "Matrix Analysis"
+						}, {
+							"name": "How Is It Done Now?"
+						}, {
+							"name": "History ans Infinitum"
+						}, {
+							"name": "If We Solve This, We Could..."
+						}, {
+							"name": "Ask All the Basic Questions"
+						}]
+					}]
+				}]
+			}]
+		}, {
+			"name": "Humans^3",
+			"children": [{
+				"name": "Cognitive Biases",
+				"children": [{
+					"name": "Fundamental Attribution Error"
+				}, {
+					"name": "Availability Bias"
+				}, {
+					"name": "Confirmation Bias"
+				}, {
+					"name": "Belief Bias"
+				}, {
+					"name": "Halo Effect"
+				}, {
+					"name": "Egocentric Bias"
+				}, {
+					"name": "Next-in-line Effect"
+				}, {
+					"name": "Shared Info Bias"
+				}, {
+					"name": "Leveling and Sharpenning"
+				}, {
+					"name": "Well-trvelled-road Efect"
+				}, {
+					"name": "Surrogation"
+				}, {
+					"name": "Survivorship Bias"
+				}, {
+					"name": "False Consensus"
+				}, {
+					"name": "Social Desirability"
+				}, {
+					"name": "Asymmetric Insight"
+				}]
+			}]
+		}, {
+			"name": "Organizations, Communities + Info Systems",
+			"children": [{
+				"name": "Community",
+				"children": [{
+					"name": "= Community of Organizations"
+				}, {
+					"name": "Organizations live on different planets"
+				}, {
+					"name": "Organizational Interaction is Awkward"
+				}, {
+					"name": "Organizations are Constrained Actors"
+				}, {
+					"name": "Organizations are Distracted Actors"
+				}, {
+					"name": "Collaboration amplifies irrationality"
+				}, {
+					"name": "Collaboration is an ambiguous techinology"
+				}, {
+					"name": "Communities are organizational junkyards"
+				}, {
+					"name": "Collaboration amplifies irrationality"
+				}]
+			}, {
+				"name": "Bureaucracy",
+				"children": [{
+					"name": "Max Weber's Six Charactaristics"
+				}, {
+					"name": "Teller's Equations"
+				}]
+			}]
+		}, {
+			"name": "Ethnography + Interviews",
+			"children": [{
+				"name": "Ethnography",
+				"children": [{
+					"name": "IS",
+					"children": [{
+						"name": "Naturalistic"
+					}, {
+						"name": "Holistic"
+					}, {
+						"name": "Descriptive"
+					}, {
+						"name": "Subjective"
+					}, {
+						"name": "Watch"
+					}, {
+						"name": "Listen"
+					}, {
+						"name": "Ask"
+					}, {
+						"name": "Participate"
+					}]
+				}, {
+					"name": "IS NOT",
+					"children": [{
+						"name": "Lab-based"
+					}, {
+						"name": "Reductionist"
+					}, {
+						"name": "Abstracted"
+					}, {
+						"name": "Outside-in"
+					}]
+				}]
+			}, {
+				"name": "Methods",
+				"children": [{
+					"name": "Free List"
+				}, {
+					"name": "Pile Sort"
+				}, {
+					"name": "Grand Tour"
+				}, {
+					"name": "Get Them Talking"
+				}, {
+					"name": "Photo Diary"
+				}, {
+					"name": "Empathy Tagging"
+				}, {
+					"name": "Unobtrusive Observing"
+				}]
+			}]
+		}, {
+			"name": "Making Sense of Qualitative Data",
+			"children": [{
+				"name": "Probrem -> How Might We...",
+				"children": [{
+					"name": "Observations"
+				}, {
+					"name": "Tag for Themes"
+				}, {
+					"name": "Insights"
+				}, {
+					"name": "P.o.V."
+				}, {
+					"name": "How Might We",
+					"children": [{
+						"name": "Amp Up the Good"
+					}, {
+						"name": "Remove the Bad"
+					}, {
+						"name": "Explore the Opposite"
+					}, {
+						"name": "Question an assumption"
+					}, {
+						"name": "Go After Adjectives"
+					}, {
+						"name": "ID Unexpected Resources"
+					}, {
+						"name": "Create an Analogy from Need or Context"
+					}, {
+						"name": "Play Against the Challenge"
+					}, {
+						"name": "Change a Status Quo"
+					}, {
+						"name": "Break POV into Pieces"
+					}]
+				}]
+			}]
+		}, {
+			"name": "Prototyping as Finding Out",
+			"children": [{
+				"name": "For What We Know",
+				"children": [{
+					"name": "Align Team Members"
+				}, {
+					"name": "Motivate You and Your Team"
+				}, {
+					"name": "Understand the Value"
+				}, {
+					"name": "Communicate the Differences"
+				}]
+			}, {
+				"name": "For What We Think We Know",
+				"children": [{
+					"name": "Test Your Assumptions"
+				}, {
+					"name": "Demonstrate the Soundness of Your Concepts"
+				}, {
+					"name": "Test Your Critical Hypotheses"
+				}]
+			}, {
+				"name": "For What We Know We Don't Know",
+				"children": [{
+					"name": "Proof of Concept"
+				}, {
+					"name": "Identify Forks in the Road"
+				}, {
+					"name": "Stepwise Refinement"
+				}, {
+					"name": "Explore Materials and Techinologies"
+				}]
+			}, {
+				"name": "For What We Don't Know We Don't Know",
+				"children": [{
+					"name": "Explore the Form"
+				}, {
+					"name": "Explore the Interaction with the Environment"
+				}, {
+					"name": "Explore the Usability"
+				}, {
+					"name": "Explore the Cultural Challenges"
+				}, {
+					"name": "Explore the Functionality"
+				}, {
+					"name": "Explore the Fabrication Challenges"
+				}, {
+					"name": "Explore the Political/Legal Challenges"
+				}]
+			}]
+		}, {
+			"name": "Contrary Views on Design",
+			"children": [{
+				"name": "DT NOT All It's Cracked Up To Be",
+				"children": [{
+					"name": "Natasha Jen",
+					"children": [{
+						"name": "Overly Perspective"
+					}, {
+						"name": "Not Cross-pollinated"
+					}, {
+						"name": "Full of Ingroup Jargon"
+					}, {
+						"name": "Reductive, Doesn't promote Skepticism"
+					}]
+				}, {
+					"name": "Iskander",
+					"children": [{
+						"name": "Privileges the Designer"
+					}, {
+						"name": "Preserves the Status-quo"
+					}, {
+						"name": "Ill-suited to Rapidly Changing Problems"
+					}]
+				}, {
+					"name": "McVey",
+					"children": [{
+						"name": "Hallmark of Good Design into Intuitive Use"
+					}]
+				}]
+			}]
+		}, {
+			"name": "Formal Methods",
+			"children": [{
+				"name": "Sampling",
+				"children": [{
+					"name": "Representativeness Matters!"
+				}, {
+					"name": "No is data too"
+				}, {
+					"name": "Randome Sample"
+				}, {
+					"name": "Sample Size"
+				}]
+			}, {
+				"name": "Concept",
+				"children": [{
+					"name": "Conservatism"
+				}, {
+					"name": "Frustration"
+				}, {
+					"name": "Value of Time"
+				}, {
+					"name": "Environment"
+				}, {
+					"name": "Willingness"
+				}]
+			}, {
+				"name": "Operationalization",
+				"children": [{
+					"name": "Observed Behaviors"
+				}, {
+					"name": "Reported Behaviors"
+				}, {
+					"name": "Reported Attitude"
+				}, {
+					"name": "Artifacts"
+				}]
+			}, {
+				"name": "Measurement",
+				"children": [{
+					"name": "Nominal"
+				}, {
+					"name": "Ordinal"
+				}, {
+					"name": "Interval"
+				}, {
+					"name": "Ratio"
+				}]
+			}, {
+				"name": "Survey Items",
+				"children": [{
+					"name": "Avoid Jargon, Vagueness, Imprecision"
+				}, {
+					"name": "Keep It Simple"
+				}, {
+					"name": "Provide Frame of Reference"
+				}, {
+					"name": "Avoid Double Barrelled Questions"
+				}, {
+					"name": "Scales Should Be Unambiguously Ordinal"
+				}, {
+					"name": "Don't Use Inflammatory Language"
+				}]
+			}]
+		}, {
+			"name": "Making Sense of Quantitative Data",
+			"children": [{
+				"name": "Probability Value",
+				"children": [{
+					"name": "True Positive",
+					"children": [{
+						"name": "Benefit of Chsnge - Cost of Change"
+					}]
+				}, {
+					"name": "False Negative",
+					"children": [{
+						"name": "Forego Benefit of Change"
+					}]
+				}, {
+					"name": "False Positive",
+					"children": [{
+						"name": "- Cost of Change"
+					}]
+				}, {
+					"name": "True Negative",
+					"children": [{
+						"name": "Status-quo Revenue"
+					}]
+				}]
+			}, {
+				"name": "Distributions",
+				"children": [{
+					"name": "Bernoulli Process"
+				}, {
+					"name": "Binomial Distribution"
+				}, {
+					"name": "Central Limit Theorem"
+				}]
+			}]
+		}]
+	}, {
+		"name": "Me",
+		"children": [{
+			"name": "Do",
+			"children": [{
+				"name": "Take HCISD"
+			}, {
+				"name": "Take other courses"
+			}, {
+				"name": "Complete all assignments"
+			}]
+		}, {
+			"name": "Think",
+			"children": [{
+				"name": "Class size is too big"
+			}, {
+				"name": "Much betther than my other courses"
+			}, {
+				"name": "Course is somewhat helpful"
+			}]
+		}, {
+			"name": "Feel",
+			"children": [{
+				"name": "Neutral"
+			}]
+		}]
+	}]
+}];
